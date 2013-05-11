41e195841ca576d4bfaa3ad848c65fa71bbd184f
Seth Albanese <salbanese0080@gmail.com>
2013-05-11 06:58:53 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaddleSportsNorthAmerica::Application.config.secret_token = '0878e7ffa6041e845177701f9c156ae53282414e23218b344b999f2c60a922189a1c729350cf89cac3541d448f61b5c58c2e6998d09246ec4941a914bad20a36'
