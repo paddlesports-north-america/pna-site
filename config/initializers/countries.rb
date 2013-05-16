02b158512fbffb56b53c13a4ad7d0f3a1653b807
Seth Albanese <salbanese0080@gmail.com>
2013-05-16 07:42:45 -0700

    prep for heroku

--- config/initializers/countries.rb
+++ config/initializers/countries.rb
@@ -1,2 +1,2 @@
-ActionView::Helpers::FormOptionsHelper::COUNTRIES = Country.order( 'name ASC' ).map { |c| [ c.name, c.id ] }
-PRIORITY_COUNTRIES = Country.where( :name => [ 'United States', 'Canada', 'Mexico' ] ).map { |c| [ c.name, c.id ] }
+# ActionView::Helpers::FormOptionsHelper::COUNTRIES = Country.order( 'name ASC' ).map { |c| [ c.name, c.id ] }
+# PRIORITY_COUNTRIES = Country.where( :name => [ 'United States', 'Canada', 'Mexico' ] ).map { |c| [ c.name, c.id ] }
