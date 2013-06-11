6aebfefdb9b80510db00fe5dbfea921a61d613a0
Seth Albanese <salbanese0080@gmail.com>
2013-06-11 12:17:26 -0700

    Added help section

---
+++ app/admin/help.rb
@@ -0,0 +1,10 @@
+ActiveAdmin.register_page "Help" do
+  menu :priority => 10
+
+  content do
+    div :class => 'help-content' do
+      h2 "Welcome to the Paddlesports North America member database."
+      para "Help content will go here"
+    end
+  end
+end
