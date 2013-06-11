042016da0b669fe088ed7f3db520586af924fcd4
Seth Albanese <salbanese0080@gmail.com>
2013-06-11 12:17:39 -0700

    added versions admin controller

---
+++ app/admin/versions.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Version do
+  menu false
+  # menu :parent => 'Settings', :label => 'History'
+
+  # index do
+  #   selectable_column
+  #   column :item
+  #   column :item_type
+  #   column :event
+  #   column :whodunnit do |v| AdminUser.find( v.whodunnit ).email unless v.whodunnit.nil? end
+  #   column :created_at
+  #   default_actions
+  # end
+end
