d634cdb00e5cdecf15b612f212a6c2bf50efc436
Seth Albanese <salbanese0080@gmail.com>
2013-06-18 07:29:32 -0700

    Added admin users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@ require File.join( default_dir, 'states.rb' )
 
 # Seed programs and awards
 require File.join( default_dir, 'programs.rb' )
+
+
+AdminUser.create( { :email => 'admin@paddlesportsnorthamerica.com', :password => 'password', :password_confirmation => 'password' } )
+AdminUser.create( { :email => 'bill.akt@gmail.com', :password => 'password', :password_confirmation => 'password' } )
+AdminUser.create( { :email => 'scottfairty@gmail.com', :password => 'password', :password_confirmation => 'password' } )
