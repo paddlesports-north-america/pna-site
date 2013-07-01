86310ed02454523632026b3a4b0801906d4ccd76
Seth Albanese <salbanese0080@gmail.com>
2013-07-01 14:02:05 -0700

    wraped countries init in rescue

--- config/initializers/countries.rb
+++ config/initializers/countries.rb
@@ -1,6 +1,10 @@
-ActionView::Helpers::FormOptionsHelper::COUNTRIES = Country.order( 'name ASC' ).map { |c| [ c.name, c.id ] }
-PRIORITY_COUNTRIES = [
-  [ 'United States', Country.find_by_name( 'United States' ).id ],
-  [ 'Canada', Country.find_by_name( 'Canada' ).id ],
-  [ 'Mexico', Country.find_by_name( 'Mexico' ).id ]
-] #Country.where( :name => [ 'United States', 'Canada', 'Mexico' ] ).map { |c| [ c.name, c.id ] }
+begin
+  ActionView::Helpers::FormOptionsHelper::COUNTRIES = Country.order( 'name ASC' ).map { |c| [ c.name, c.id ] }
+  PRIORITY_COUNTRIES = [
+    [ 'United States', Country.find_by_name( 'United States' ).id ],
+    [ 'Canada', Country.find_by_name( 'Canada' ).id ],
+    [ 'Mexico', Country.find_by_name( 'Mexico' ).id ]
+  ] #Country.where( :name => [ 'United States', 'Canada', 'Mexico' ] ).map { |c| [ c.name, c.id ] }
+rescue
+  Rails.log.warn "Failed to initialize countries constant"
+end
