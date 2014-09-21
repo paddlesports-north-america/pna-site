9bc915e7b92c4ddc382dec04428d752a3bfcbf51
Seth Albanese <seth@skeg-works.com>
2014-09-21 15:12:55 -0700

    added http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with name: "pna", password: "review2014"
+  http_basic_authenticate_with name: "pna", password: "review2014"
   protect_from_forgery
 end
