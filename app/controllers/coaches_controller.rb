e475abcb1799ccfcd8fc9ff846ce945eeed08838
Seth Albanese <seth@skeg-works.com>
2015-10-05 10:45:16 -0700

    fixed error on membershp check for coahces page

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,6 +1,6 @@
 class CoachesController < ApplicationController
   def index
-    @coaches = Member.includes( :awards ).where( :show_on_coaches_page => true, 'awards.award_type' => 'coaching' ).group_by { |coach|
+    @coaches = Member.includes( :awards ).where( :show_on_coaches_page => true, 'awards.award_type' => 'coaching' ).select{ |m| m.membership_status != :error }.group_by { |coach|
       unless coach.addresses.empty?
         unless coach.addresses.first.state.nil?
           coach.addresses.first.state
@@ -8,7 +8,7 @@ class CoachesController < ApplicationController
           coach.addresses.first.country
         end
       end
-    }.select{ |m| m.membership_status != :error }.sort_by { |k,v| k.name }
+    }.sort_by { |k,v| k.name }
     
     @regions = Region.all #.order( :name )
   end
