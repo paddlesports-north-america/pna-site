6565ca04f6eb22058ebbda892b2707a5eaea92d1
Seth Albanese <seth@skeg-works.com>
2015-10-05 10:03:54 -0700

    resolved url formating on coaches page

--- app/helpers/coaches_helper.rb
+++ app/helpers/coaches_helper.rb
@@ -1,11 +1,11 @@
 module CoachesHelper
-  include Addressable 
+  
   
   def region_coaches_by_local( region, coaches )
     coaches.select { |l,_| region.locations.include? l }
   end
   
   def domain_name(uri)
-    Addressable::URI.heuristic_parse(uri, :scheme => "http").host[/\w+\.\w+(\.\w{2})?\Z/]
+    URI.parse(uri).host[/\w+\.\w+(\.\w{2})?\Z/]
   end
 end
