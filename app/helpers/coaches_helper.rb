6cfa833463aab00268fc5b112ccbd3ced499ee53
Seth Albanese <seth@skeg-works.com>
2015-10-05 09:27:13 -0700

    pretty url formats for coaches page

--- app/helpers/coaches_helper.rb
+++ app/helpers/coaches_helper.rb
@@ -3,4 +3,8 @@ module CoachesHelper
   def region_coaches_by_local( region, coaches )
     coaches.select { |l,_| region.locations.include? l }
   end
+  
+  def domain_name(uri)
+    Addressable::URI.heuristic_parse(uri, :scheme => "http").host[/\w+\.\w+(\.\w{2})?\Z/]
+  end
 end
