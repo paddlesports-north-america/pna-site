6e1a522fb7f3a01a4a5b8a7af32fba1fc81cd585
Seth Albanese <seth@skeg-works.com>
2016-01-23 10:12:54 -0800

    sorting coaches by last name on coaches page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,6 +2,10 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
+  content do 
+    h1 'Remember, with great power comes great responsibility!'
+  end
+
   # content :title => proc{ I18n.t("active_admin.dashboard") } do
   #   columns do
   #     column do
--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,6 +1,6 @@
 class CoachesController < ApplicationController
   def index
-    @coaches = Member.includes( :awards ).where( :show_on_coaches_page => true, 'awards.award_type' => 'coaching' ).select{ |m| m.membership_status != :error }.group_by { |coach|
+    @coaches = Member.includes( :awards ).where( :show_on_coaches_page => true, 'awards.award_type' => 'coaching' ).order( :last_name ).select{ |m| m.membership_status != :error }.group_by { |coach|
       unless coach.addresses.empty?
         unless coach.addresses.first.state.nil?
           coach.addresses.first.state
