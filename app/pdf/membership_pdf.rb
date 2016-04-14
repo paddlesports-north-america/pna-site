15bf280bfc56d74a91880d31f3cab96fe9f57bfb
Seth Albanese <seth@skeg-works.com>
2016-04-14 12:35:57 -0700

    color membership cards

--- app/pdf/membership_pdf.rb
+++ app/pdf/membership_pdf.rb
@@ -4,10 +4,10 @@ class MembershipPdf < Prawn::Document
 
   PAGE_SIZE = Prawn::Document::PageGeometry::SIZES["LETTER"]
   TEMPLATES = {
-                :header => File.join( Rails.root, 'private', 'pna-membership-header-bw.png' ),
+                :header => File.join( Rails.root, 'private', 'pna-membership-header.png' ),
                 :card => {
-                  :charter => File.join( Rails.root, 'private', "pna-charter-member-card-bw.png"),
-                  :standard => File.join( Rails.root, 'private', "pna-member-card-bw.png")
+                  :charter => File.join( Rails.root, 'private', "pna-charter-member-card.png"),
+                  :standard => File.join( Rails.root, 'private', "pna-member-card.png")
                 }
               }
   MARGIN = -0.5.in
@@ -26,19 +26,19 @@ class MembershipPdf < Prawn::Document
       'Your membership qualifies you as an ambassador for PNA. Please spread the word about PNA courses and help us recruit new members!'
     ]
   }
-  
+
   COLOR = '4C4D4F'
 
   def to_pdf( membership )
 
     image TEMPLATES[ :header ], :at => [ 0.86.in, PAGE_SIZE[1] - 0.5625.in ], :fit => [ PAGE_SIZE[ 0 ] - 0.86.in * 2, PAGE_SIZE[ 1 ] - 1.14.in ]
-    
-    
+
+
     bounding_box( [ 1.1.in, PAGE_SIZE[1] - 1.9.in ], :width => PAGE_SIZE[0] - 2.2.in ) do
-      
+
       make_font :lead, 21
       text COPY[ :lead ], :align => :center, :color => COLOR
-      
+
       move_down 0.25.in
       make_font :regular, 13
       COPY[ :body ].each do |c|
@@ -47,28 +47,28 @@ class MembershipPdf < Prawn::Document
       end
     end
     card = membership.member.is_charter_member? ? TEMPLATES[ :card ][ :charter ] : TEMPLATES[ :card ][ :standard ]
-    
+
     image card, :at => [ 0.86.in, PAGE_SIZE[1] - 8.17.in ], :fit => [ PAGE_SIZE[ 0 ] - 0.86.in * 2, PAGE_SIZE[ 1 ] - 1.14.in ]
-    
+
   # Member Info
     bounding_box( [ 4.5.in, 2.5.in ], :width => 2.86.in, :height => 1.61.in ) do
       make_font :regular, 18
       text "#{membership.type} Member", :align => :center, :color => COLOR
-      
+
       move_down 0.26.in
       make_font :regular, 10
       text "#{membership.member.first_name} #{membership.member.last_name}", :align => :center, :color => COLOR
-      
+
       # move_down 0.05.in
       text "Valid Until #{membership.expiration_date.strftime( "%m/%d/%Y" )}", :align => :center, :color => COLOR
-      
+
       move_down 0.15.in
       make_font :regular, 24
       text sprintf( "%06d", membership.member.id ), :align => :center, :color => COLOR
     end
   #END member info
-    
-    
+
+
 
     render
   end
