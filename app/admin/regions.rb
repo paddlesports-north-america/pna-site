35b6ee92bb8125532dde834c363890196602808e
Seth Albanese <seth@skeg-works.com>
2016-04-23 08:03:13 -0700

    autocomplete for regional reps

--- app/admin/regions.rb
+++ app/admin/regions.rb
@@ -3,6 +3,17 @@ ActiveAdmin.register Region do
 
   config.batch_actions = false
 
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :rco_id, :as => :string,
+        :label => 'Regional Representative',
+        :input_html => member_autocomplete_options.merge( { :multiple => false } )
+
+      f.input :contact, :label => 'Representative Contact'
+    end
+    f.actions
+  end
 end
 
 ActiveAdmin.register RegionLocation do
