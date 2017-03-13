abb9ee5a6a77d94be5bd979142db1b28d510392e
Rick Kitts <rkitts@guidewire.com>
2017-03-13 15:26:59 -0700

    First pass at changes to cert printing. If in dev mode (i.e. !Rails.env.production?) then we print on a scan of the template so that a developer can see where things are being printed on the page.
    This change only encompasses endorsements and performance awards.

--- app/pdf/endorsement_pdf.rb
+++ app/pdf/endorsement_pdf.rb
@@ -2,15 +2,34 @@ class EndorsementPdf < Prawn::Document
 
   require 'prawn/measurement_extensions'
 
-  # PAGE_SIZE = [ 6.in, 8.25.in ]
   PAGE_SIZE = [ 8.25.in, 11.69.in ]
   MARGIN = 0
 
   def initialize()
-    super( :page_size => PAGE_SIZE, :margin => MARGIN )
+    if Rails.env.production?
+      bg_img = nil
+    else
+      bg_img = "#{Rails.root}/private/attendance-certificate-blank.png"
+    end
+    super( :page_size => PAGE_SIZE, :margin => MARGIN, :background => bg_img)
   end
 
   def to_pdf( params )
+
+    bounding_box( [ 0.25.in, 10.8.in ], :width => 6.25.in, :height => 6.in ) do
+      font File.join( Rails.root, 'fonts', 'Arial.ttf' )
+      font_size 12
+      text Date.today.strftime( '%B %d, %Y' )
+      move_down 0.25.in
+      text "Dear #{params[ :member ].first_name}"
+
+      move_down 0.25.in
+
+      text "Congratulations on having passed your training course. Thousands of paddlers have taken part in BCU programs in North America. Whatever your paddlesports objective, there is something for everyone, which is why paddlesports continues to be the most popular watersport."
+
+      move_down 0.25.in
+      text "Information about PNA membership and member benefits can be found on the PNA website at http://www.paddlesportsnorthamerica.org. If you have any questions about the programs we offer, please contact your PNA Regional Representative. You can find your Regional Representative by visiting the Coaches page on PNA's website: http://www.paddlesportsnorthamerica.org/coaches"
+     end
 
     font File.join( Rails.root, 'fonts', 'Arial Bold.ttf' )
 
@@ -30,9 +49,6 @@ class EndorsementPdf < Prawn::Document
       text params[ :date ].strftime( '%B %d, %Y' ), :align => :center
     end
 
-    bounding_box( [ 6.in, 0.75.in ], :width => 2.25.in, :height => 1.in ) do
-      text "#{params[ :member ].id.to_s.rjust( 4, '0' )}#{params[:course].id.to_s.rjust(4,'0')}", :align => :center, :character_spacing => 1.25
-    end
     render
   end
 
